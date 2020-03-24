1dce1e383a22275ccb5cc35806f979c4cee246a4
Brian V. Hughes <brianvh89@gmail.com>
2020-03-24 13:33:36 -0700

    Add Dry::Types and BaseStruct to library file

--- lib/mirren.rb
+++ lib/mirren.rb
@@ -1,6 +1,28 @@
 require 'mirren/version'
+require 'dry-struct'
+
+module Types
+  include Dry.Types()
+  NilOrString = Types::Nil | Types::String
+  FloatString = Types.Constructor(Float) { |v| v.to_f }
+  IntString = Types.Constructor(Integer) { |v| v.to_i }
+  YesBool = Types.Constructor(Bool) { |v| v.downcase == 'yes' ? true : false }
+  HashTypes = Types::String.enum('hash', 'kh', 'mh', 'gh', 'th')
+end
 
 module Mirren
-  class Error < StandardError; end
-  # Your code goes here...
+  Error = Class.new(StandardError)
+  class ApiError < Error
+    def initialize(message)
+      super("API Error: #{message}")
+    end
+  end
+
+  class ParamsError < Error
+    def initialize(klass)
+      super("Params Error: You must supply a #{klass} to this API method")
+    end
+  end
+
+  BaseStruct = Dry.Struct { transform_keys(&:to_sym) }
 end
