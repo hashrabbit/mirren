086c5e759c69c3eeb122128b116c9eab70289931
Mark Schifflin <randy.schifflin@gmail.com>
2020-04-08 14:33:50 -0700

    Spec out rig endpoints

--- lib/mirren/rigs/endpoints.rb
+++ lib/mirren/rigs/endpoints.rb
@@ -8,13 +8,31 @@ module Mirren
       def fetch_rigs(params: nil)
         valid_params!(params, Params)
 
-        fields = get('/rig', params: params.to_h)
-        Found.new(fields)
+        get('/rig', params: params.to_h).bind do |fields|
+          Found.try(fields)
+            .to_monad.extend(ResultExt)
+            .fmap_left { UnmarshalError(_1) }
+        end
+      end
+
+      def fetch_rigs!(kwargs)
+        fetch_rigs(**kwargs)
+          .extend(ResultExt)
+          .unwrap_result!
       end
 
       def fetch_rig(id:)
-        fields = get("/rig/#{id}")
-        Rig.new(fields)
+        get("/rig/#{id}").bind do |fields|
+          Rig.try(fields)
+            .to_monad.extend(ResultExt)
+            .fmap_left { UnmarshalError(_1) }
+        end
+      end
+
+      def fetch_rig!(kwargs)
+        fetch_rig(**kwargs)
+          .extend(ResultExt)
+          .unwrap_result!
       end
     end
   end
--- spec/rigs/endpoints_spec.rb
+++ spec/rigs/endpoints_spec.rb
@@ -3,29 +3,147 @@ require 'spec_helper'
 module Mirren
   module Rigs
     RSpec.describe Endpoints do
-      let(:client) { Class.new.tap { |c| c.extend Endpoints } }
-      let(:params) {
-        Params.new(type: 'sha256ab', count: 10, rpi: { min: 100 })
-      }
-      let(:full_params) {
-        Params.new(
-          type: 'sha256', count: 10, rpi: { min: 100 },
-          minhours: { max: 24 }, hash: { min: 12, max: 16, type: 'th' }
-        )
-      }
-      let(:found) { client.fetch_rigs(params: full_params) }
-      let(:xnonce) { found.records.select(&:xnonce) }
+      let(:request) { double() }
+      let(:client) { MockClient.new(Endpoints).call(request) }
+      let(:response) { "{ \"success\": #{response_success?}, \"data\": #{response_data} }" }
 
-      describe '#fetch_rigs(:params)' do
-        it 'filter Rigs based on :type and additionally supplied Params' do
-          expect(found.total).to be > 0
-        end
+      before do
+        allow(request).to receive(:call) { Struct.new(:body).new(response) }
       end
 
-      describe '#fetch_rig(:id)' do
-        it 'returns details for the Rig specified by :id' do
-          rig = client.fetch_rig(id: found.records[0].id)
-          expect(rig.rpi).to eq 100.0
+      context 'when the underlying request returns without success' do
+        include_context 'endpoint failures', {
+          fetch_rigs: {params: Params.new({
+            type: 'sha256ab',
+            count: 10,
+            rpi: { min: 100 }
+          })},
+          fetch_rig: {id: '3'}
+        }
+      end
+
+      context 'when the underlying request succeeds' do
+        let(:response_success?) { true }
+        describe '#fetch_rigs(:params)' do
+          let(:full_params) {
+            Params.new(
+              type: 'sha256', count: 10, rpi: { min: 100 },
+              minhours: { max: 24 }, hash: { min: 12, max: 16, type: 'th' }
+            )
+          }
+          let(:response_data) do
+            <<-JSON
+            {
+              "offset": "3",
+              "count": "10",
+              "total": "100",
+              "stats": {
+                "available": {
+                  "rigs": "3",
+                  "hash": {
+                    "hash": "10.0",
+                    "unit": "stats_available_rigs_unit",
+                    "type": "stats_available_rigs_type",
+                    "nice": "stats_available_rigs_nice"
+                  }
+                },
+                "rented": {
+                  "rigs": "6",
+                  "hash": {
+                    "hash": "20.0",
+                    "unit": "stats_rented_rigs_unit",
+                    "type": "stats_rented_rigs_type",
+                    "nice": "stats_rented_rigs_nice"
+                  }
+                },
+                "prices": {
+                  "lowest": "10.0",
+                  "last_10": "20.0",
+                  "last": "25.0"
+                }
+              },
+              "records": []
+            }
+            JSON
+          end
+
+          it 'filter Rigs based on :type and additionally supplied Params' do
+            found = client.fetch_rigs(params: full_params).value!
+            expect(found).to be_a Found
+          end
+        end
+
+        describe '#fetch_rig(:id)' do
+          let(:response_data) do
+            <<-JSON
+            {
+                "id": "1",
+                "name": "rig_name",
+                "owner": "rig_owner",
+                "type": "rig_type",
+                "status": {
+                  "status": "rig_status",
+                  "hours": "24",
+                  "rented": true,
+                  "online": true
+                },
+                "online": true,
+                "xnonce": "yes",
+                "ndevices": 8,
+                "region": "rig_region",
+                "rpi": 4.5,
+                "suggested_diff": "100.0",
+                "optimal_diff": {
+                  "min": "50.0",
+                  "max": "500.0"
+                },
+                "poolstatus": "rig_poolstatus",
+                "extensions": true,
+                "price": {
+                  "type": "rig_price_type",
+                  "BTC": {
+                    "currency": "rig_price_BTC_currency",
+                    "price": "100.0",
+                    "hour": "12.0",
+                    "minhrs": "24.0",
+                    "maxhrs": "72.0",
+                    "enabled": true
+                  }
+                },
+                "minhours": "24",
+                "maxhours": "72",
+                "hashrate": {
+                  "advertised": {
+                    "hash": "100.0",
+                    "type": "rig_hashrate_advertised_type",
+                    "nice": "rig_hashrate_advertised_nice"
+                  },
+                  "last_5min": {
+                    "hash": "200.0",
+                    "type": "rig_hashrate_last_5min_type",
+                    "nice": "rig_hashrate_last_5min_nice"
+                  },
+                  "last_15min": {
+                    "hash": "300.0",
+                    "type": "rig_hashrate_last_15min_type",
+                    "nice": "rig_hashrate_last_15min_nice"
+                  },
+                  "last_30min": {
+                    "hash": "300.0",
+                    "type": "rig_hashrate_last_30min_type",
+                    "nice": "rig_hashrate_last_30min_nice"
+                  }
+                },
+                "shorturl": "rig_shorturl",
+                "description": "rig_description"
+              }
+            JSON
+          end
+
+          it 'returns details for the Rig specified by :id' do
+            rig = client.fetch_rig(id: '3').value!
+            expect(rig).to be_a Rig
+          end
         end
       end
     end
