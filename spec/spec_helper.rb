a06a9a0c7a5815e624eb8edb32f0da9d92919481
Mark Schifflin <randy.schifflin@gmail.com>
2020-04-14 15:52:43 -0700

    Move mock client to spec support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,22 +31,4 @@ RSpec.configure do |config|
   end
 end
 
-class MockClient
-  @@klasses = {}
-
-  def initialize(endpoints_klass)
-    @klass = @@klasses[endpoints_klass] ||= Class.new do
-      include endpoints_klass
-      attr_reader :request
-      def initialize(request)
-        @request = request
-      end
-    end
-  end
-
-  def call(request)
-    @klass.new(request)
-  end
-end
-
 Dir['./spec/support/**/*.rb'].sort.each { require _1 }
