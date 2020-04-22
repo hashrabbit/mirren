3b21db387d1bddf1dd7d60828b500fd86c04983c
Brian V. Hughes <brianvh89@gmail.com>
2020-04-22 13:29:27 -0700

    Add Api::Response and Do notation to Api::Request
    This is a full refactoring of the Request class, converting the #call
    method with monad Do notation. I think this provides a more explicit
    way to integrate the "expected" Exceptions, leaving a single rescue
    block, where we had 3.
    Also replaces the #initialize method with Dry::Initializer params.
    Add a new integration example to client_spec.rb that verifies a Failure
    from a Api::Response::Status payload, with a false :success entry.

--- lib/mirren/api/request.rb
+++ lib/mirren/api/request.rb
@@ -4,43 +4,45 @@ require 'rest-client'
 module Mirren
   module Api
     class Request
-      include Dry::Monads[:result]
+      include Dry::Monads[:result, :try]
+      include Dry::Monads::Do.for(:call)
+      extend Dry::Initializer
 
-      attr_reader :host, :auth, :method, :request, :response
-
-      def initialize(host, auth, method, request)
-        @host = host
-        @auth = auth
-        @method = method
-        @request = request
-      end
+      param :host
+      param :auth
+      param :method
+      param :request
 
       def call(path, get_params: {}, post_params: {})
-        body = JSON.parse(
-          response(path, get_params, post_params).body
-        )
-        return Success(body['data']) if body['success']
-
-        Failure(ApiError.new(body['data']['message']))
-      rescue RestClient::ExceptionWithResponse => e
-        Failure(ApiError.new(e))
+        req_opts = request_opts(path, get_params, post_params)
+        @response = yield perform(req_opts)
+        body = yield parse(@response.body)
+        Response.new(body).to_result
       rescue RestClient::Exception => e
         Failure(ClientError.new(e))
-      rescue JSON::ParserError => e
-        Failure(JsonError.new(e))
       end
 
       private
 
-      def response(path, get_params, post_params)
-        @response ||= request.call(
-          {
-            method: method,
-            url: "#{host}#{path}",
-            headers: Header.call(auth, path).merge(params: get_params),
-            payload: post_params && post_params.to_json
-          }.compact
-        )
+      def perform(opts)
+        Try(RestClient::ExceptionWithResponse) { request.call(opts) }
+          .to_result
+          .or { Failure(ApiError.new(_1)) }
+      end
+
+      def parse(json)
+        Try(JSON::ParserError) { JSON.parse(json) }
+          .to_result
+          .or { Failure(JsonError.new(_1)) }
+      end
+
+      def request_opts(path, get_params, post_params)
+        {
+          method: method,
+          url: "#{host}#{path}",
+          headers: Header.call(auth, path).merge(params: get_params),
+          payload: post_params && post_params.to_json
+        }.compact
       end
     end
   end
